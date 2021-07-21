8e6239d70fa9d8b935ec404bde3eccb678798196
ksenia <ksenia@bekitzur.com>
2021-07-21 10:54:43 +0300

    added FeedLoadingErrorViewModel

---
+++ MVP Module/Source/Feed Presentation/FeedLoadingErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingErrorViewModel.swift
+//  MVP
+//
+//  Created by Ksenia on 21.07.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedLoadingErrorViewModel {
+	let error: String
+}
